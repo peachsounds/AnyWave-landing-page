e85a7ab13d094dee385b4ec17e83da67800ee39e
sareldu <sareldu@gmail.com>
2026-01-28 23:37:51 +0200

    Update PostHog API key to new clean project

--- webhook-worker/worker.js
+++ webhook-worker/worker.js
@@ -6,7 +6,7 @@
  */
 
 // PostHog configuration
-const POSTHOG_API_KEY = 'phc_UQ72wbfMQnS4IWPhPI28A0zeHQJjBW15oCzMeb1vyx8';
+const POSTHOG_API_KEY = 'phc_Diy4fB5dTnxjkxCneauukkmdVkfw1YRm6EAejL45PG3';
 const POSTHOG_HOST = 'https://us.i.posthog.com';
 
 // Lemon Squeezy webhook secret (set this in Cloudflare dashboard as environment variable)
